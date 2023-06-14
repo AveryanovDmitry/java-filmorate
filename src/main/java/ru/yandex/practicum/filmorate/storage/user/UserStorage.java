cdc12203cab681ed0d4dabf6c435b186b8dea9c0
Dmitry Averyanov <averyanov-22@yandex.ru>
2023-06-14 11:07:00 +0300

    checkstyle после исправления конфликтов1

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -22,6 +22,6 @@ public interface UserStorage {
     Collection<User> getCommonFriendsList(User user, User other);
 
     Collection<Integer> getUserFilmIdsRecommendations(int userId);
-  
+
     void deleteUserById(int id);
 }
